f1785a3c69e15dc13aeaea54fa9bd7f0a32e7b9b
justin <jhp9@rice.edu>
2025-03-26 14:31:06 -0500

    Changes to secret aws key

--- src/components/awsconfig.js
+++ src/components/awsconfig.js
@@ -1,12 +1,12 @@
 import AWS from "aws-sdk";
 
 AWS.config.update({
-  accessKeyId: "AKIAVY2PHBHNZ3CCRHWE", // Replace with your IAM Access Key
-  secretAccessKey: "JBCqjT1iDSV+LhyrtVxDYTe0dus+dZw8SeWb3gRa", // Replace with your IAM Secret Key
-  region: "us-east-2", // Change to your AWS region
+  accessKeyId: process.env.REACT_APP_AWS_ACCESS_KEY_ID,
+  secretAccessKey: process.env.REACT_APP_AWS_SECRET_ACCESS_KEY,
+  region: process.env.REACT_APP_AWS_REGION,
 });
 
 const s3 = new AWS.S3();
-const S3_BUCKET = "balding"; // Replace with your S3 bucket name
+const S3_BUCKET = "balding"; // or your actual bucket name
 
 export { s3, S3_BUCKET };
