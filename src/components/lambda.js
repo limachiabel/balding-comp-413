52d08f2b4cf839611060efa76e83ab340b49e04a
Jesus Daniel Terrazas <jdt11@rice.edu>
2025-03-26 21:31:13 -0500

    updated fetch body to work with lambda

--- src/components/lambda.js
+++ src/components/lambda.js
@@ -1,15 +1,13 @@
 export default async function lambda() {
-        const response = await fetch("https://6rv245iay2fpbxjiujvd3bi36a0kxivh.lambda-url.us-east-2.on.aws/", {
+        const response = await fetch("https://6rv245iay2fpbxjiujvd3bi36a0kxivh.lambda-url.us-east-2.on.aws/",
+        {
           method: "POST",
           headers: {
             "Content-Type": "application/json"
           },
-          body: {
-            img_path: "string"
-          }
+          body: JSON.stringify({'img_path': 'uploads/image_0001.jpg'})
         });
       
         const data = await response.json();
         console.log(data);
       }
-      
